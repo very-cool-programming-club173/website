d4b9fb431ab0d3131f367d2bad4ccab0cee87c17
Jayden <skewed-fade-deluxe@duck.com>
2025-02-20 12:24:36 +0100

    feat: Fix docosaurus build

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -9,10 +9,10 @@ const config: Config = {
   tagline: "programming is cool",
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
+  url: "https://very-cool-programming-club173.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/website/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
